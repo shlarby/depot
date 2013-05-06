d203d140f0213059048a0ea217e61261b0724a53
jerry <jerry.napitupulu@gmail.com>
2013-05-06 22:17:33 +0200

    Validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-#---
+
+<!-- saved from url=(0072)http://media.pragprog.com/titles/rails4/code/rails32/depot_a/db/seeds.rb -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"><style type="text/css"></style><script>window["_GOOG_TRANS_EXT_VER"] = "1";</script></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">#---
 # Excerpted from "Agile Web Development with Rails",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material, 
@@ -10,36 +12,37 @@
 Product.delete_all
 Product.create(title: 'CoffeeScript',
   description: 
-    %{<p>
+    %{&lt;p&gt;
         CoffeeScript is JavaScript done right. It provides all of JavaScript's
 	functionality wrapped in a cleaner, more succinct syntax. In the first
 	book on this exciting new language, CoffeeScript guru Trevor Burnham
 	shows you how to hold onto all the power and flexibility of JavaScript
 	while writing clearer, cleaner, and safer code.
-      </p>},
+      &lt;/p&gt;},
   image_url:   'cs.jpg',    
   price: 36.00)
 # . . .
 Product.create(title: 'Programming Ruby 1.9',
   description:
-    %{<p>
+    %{&lt;p&gt;
         Ruby is the fastest growing and most exciting dynamic language
         out there. If you need to get working programs delivered fast,
         you should add Ruby to your toolbox.
-      </p>},
+      &lt;/p&gt;},
   image_url: 'ruby.jpg',
   price: 49.95)
 # . . .
 
 Product.create(title: 'Rails Test Prescriptions',
   description: 
-    %{<p>
-        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
+    %{&lt;p&gt;
+        &lt;em&gt;Rails Test Prescriptions&lt;/em&gt; is a comprehensive guide to testing
         Rails applications, covering Test-Driven Development from both a
         theoretical perspective (why to test) and from a practical perspective
         (how to test effectively). It covers the core Rails testing tools and
         procedures for Rails 2 and Rails 3, and introduces popular add-ons,
         including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
-      </p>},
+      &lt;/p&gt;},
   image_url: 'rtp.jpg',
   price: 34.95)
+</pre></body></html>
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -1,49 +1,82 @@
+
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 require 'test_helper'
 
-class ProductsControllerTest < ActionController::TestCase
-  setup do
-    @product = products(:one)
+class ProductTest < ActiveSupport::TestCase
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:products)
+  test "product price must be positive" do
+    product = Product.new(title:       "My Book Title",
+                          description: "yyy",
+                          image_url:   "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], 
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
+  def new_product(image_url)
+    Product.new(title:       "My Book Title",
+                description: "yyy",
+                price:       1,
+                image_url:   image_url)
   end
 
-  test "should create product" do
-    assert_difference('Product.count') do
-      post :create, product: @product.attributes
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
     end
 
-    assert_redirected_to product_path(assigns(:product))
-  end
-
-  test "should show product" do
-    get :show, id: @product
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @product
-    assert_response :success
-  end
-
-  test "should update product" do
-    put :update, id: @product, product: @product.attributes
-    assert_redirected_to product_path(assigns(:product))
-  end
-
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete :destroy, id: @product
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
-
-    assert_redirected_to products_path
   end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:       products(:ruby).title,
+                          description: "yyy", 
+                          price:       1, 
+                          image_url:   "fred.gif")
+
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title:       products(:ruby).title,
+                          description: "yyy", 
+                          price:       1, 
+                          image_url:   "fred.gif")
+
+    assert product.invalid?
+    assert_equal [I18n.translate('activerecord.errors.messages.taken')],
+                 product.errors[:title]
+  end
+  
 end
