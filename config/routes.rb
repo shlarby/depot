c84fb367dd2c368f2fcd4de7a2671041341adf62
jerry <jerry.napitupulu@gmail.com>
2013-05-07 00:17:39 +0200

    Cart creation

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 Depot::Application.routes.draw do
+  resources :line_items
+
+
+  resources :carts
+
+
   get "store/index"
 
   resources :products
